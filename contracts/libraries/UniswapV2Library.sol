5761f2f8a12ef1902091d270976a14bde268d84f
Eason <xuyihaoo@gmail.com>
2023-03-04 10:06:55 +0800

    change init code hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'2f89ab6852fbc8e271b2d58749c8d3d966dc707f5525706cbcb2416ad25648aa' // init code hash
             ))));
     }
 
